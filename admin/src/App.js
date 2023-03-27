48472e1f2ae60271d22ac16e75a155f888be13d9
loc311 <bbaoloc311@gmail.com>
2023-03-27 14:18:03 +0700

    user

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,45 +1,106 @@
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Navigate, Outlet } from "react-router-dom";
 import { productInputs, userInputs } from "./formSource";
 import "./style/dark.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthContext } from "./context/AuthContext";
+import Sidebar from "./components/sidebar/Sidebar";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
+
+  const { currentUser } = useContext(AuthContext);
   const { darkMode } = useContext(DarkModeContext);
 
+  const queryClient = new QueryClient();
+
+  const Layout = () => {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <div className={`theme-${darkMode ? "dark" : "light"}`}>
+          <Navbar />
+          <div style={{ display: "flex" }}>
+            <Sidebar />
+            <div style={{ flex: 6 }}>
+              <Outlet />
+            </div>
+            
+          </div>
+        </div>
+      </QueryClientProvider>
+    );
+  };
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children:[
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/user/:id",
+          element: <Single />,
+        },
+        {
+          path: "/user",
+          element: <List inputs={userInputs} title="Users" />,
+        },
+        {
+          path: "/product/:id",
+          element: <Single />,
+        },
+        {
+          path: "/product",
+          element: <List inputs={productInputs} title="Products" />,
+        },
+        // {
+        //   path: "/order/:id",
+        //   element: <Single />,
+        // },
+        // {
+        //   path: "/order",
+        //   element: <List inputs={orderInputs} title="Orders" />,
+        // },
+      ]
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
   return (
-    <div className={darkMode ? "app dark" : "app"}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Home />} />
-            <Route path="login" element={<Login />} />
-            <Route path="users">
-              <Route index element={<List />} />
-              <Route path=":userId" element={<Single />} />
-              <Route
-                path="new"
-                element={<New inputs={userInputs} title="Add New User" />}
-              />
-            </Route>
-            <Route path="products">
-              <Route index element={<List />} />
-              <Route path=":productId" element={<Single />} />
-              <Route
-                path="new"
-                element={<New inputs={productInputs} title="Add New Product" />}
-              />
-            </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
-}
+   
+};
 
 export default App;
--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -1,43 +1,87 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 import "./login.scss";
 
 const Login = () => {
 
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+);
 
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
 
-  //   axios.post('/api/auth/login', {
-  //     username: username,
-  //     password: password
-  //   })
-  //   .then(response => {
-  //     localStorage.setItem('accessToken', response.data.accessToken);
-  //     // Redirect to protected page
-  //   })
-  //   .catch(error => {
-  //     console.log(error);
-  //   });
-  // };
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [err, setErr] = useState("");
+
+  const navigate = useNavigate();
+
+  const { setAuthData } = useContext(AuthContext);
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const { login } = useContext(AuthContext);
+
+  // const handleLogin = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     const response = await login(inputs);
+
+  //     const { token } = response.data;
+  //     localStorage.setItem("token", token);
+  //     navigate("/")
+  //   } catch (err) {
+  //     // setErr(err.response.data);
+  //   }
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:8888/api/v1/authenticate", inputs);
+      const { token, user } = res.data;
+      localStorage.setItem("user", JSON.stringify({ token, user }));
+      setCurrentUser({ token, user });
+      navigate("/");
+    } catch (err) {
+      setErr(err.response.data);
+    }
+
+  };
+
+
   return (
     <div className='login'>
       <div className="card">
         <div className="left">
-            <h1>Hello!</h1>
-            <p>lasnckjcakjcn</p>
-            <span>Bạn không có tài khoản ?</span>
-            <Link to="/register">
-                        <button>Đăng Kí</button>
-                    </Link>
+          <h1>Hello!</h1>
+          <p>lasnckjcakjcn</p>
+          <span>Bạn không có tài khoản ?</span>
+          <Link to="/register">
+            <button>Đăng Kí</button>
+          </Link>
         </div>
         <div className="right">
-            <h1>Login</h1>
-            <form>
-                <input type="text" placeholder='User' />
-                <input type="password" placeholder='Password' />
-                <button>Login</button>
-            </form>
+          <h1>Login</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
+            {err && err}
+            <button onClick={handleLogin}>Login</button>
+          </form>
         </div>
       </div>
     </div>
